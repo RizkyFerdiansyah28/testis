b4b38b8df4b22d1b2a56d3360a379d1fbe69a06a
Rizky Ferdiansyah <87290766+RizkyFerdiansyah28@users.noreply.github.com>
2024-02-10 10:47:45 +0700

    Add files via upload

---
+++ array5/array5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukkan angka: ");
+            int q = int.Parse(Console.ReadLine());
+            int[] a = new int[q];
+            int r = 0;
+            for (int i = 1; i <= q; i++)
+            {
+                Console.Write($"a[{i}]=");
+                a[i - 1] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"total {a.Average()}");
+            Console.ReadLine();
+        }
+    }
+}
